310a7ea2e5c1625c85a6fea1c1900bcbd18bdbab
keme686 <keme686@gmail.com>
2015-11-26 13:44:11 +0100

    inital commit

---
+++ src/main/java/de/unibonn/iai/eis/slicer/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package de.unibonn.iai.eis.slicer;
+
+/**
+ * @author Kemele M. Endris
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
